c55bee0b87ad7a925dbdbb2c798e3f2fac463efa
Elijah Obisesan <elijahobisesan01@gmail.com>
2023-09-07 23:19:27 +0100

    Done

---
+++ script.js
@@ -0,0 +1,63 @@
+// // function displayCurrentTimeUTC() {
+// //     const currentTimeUTC = new Date().toUTCString();
+// //     console.log(currentTimeUTC);
+// //   }
+// //   displayCurrentTimeUTC();
+// function displayTime() {
+//   let utcDisplay = document.getElementById("utc");
+//   let dayOfWeek = document.getElementById("day");
+//   let d = new Date();
+//   function setTime() {
+//     const days = [
+//       "Sunday",
+//       "Monday",
+//       "Tuesday",
+//       "Wednesday",
+//       "Thursday",
+//       "Friday",
+//       "Saturday",
+//     ];
+//     dayOfWeek.innerHTML = days[d.getDay()];
+//     let currentTimeUTC = new Date().toUTCString();
+//     function updateTime() {
+//       utcDisplay.innerHTML = currentTimeUTC;
+//     //   setInterval(currentTimeUTC, 1000);
+//     }
+//     updateTime()
+//   }
+//   setTime()
+// //   setInterval(updateTime, 1000)
+// }
+// displayTime();
+
+// // utcDisplay.innerHTML = currentTimeUTC;
+// // dayOfWeek.innerHTML = day;
+// // setInterval(currentTimeUTC, 1000)
+
+function displayTime() {
+  let utcDisplay = document.getElementById("utc");
+  let dayOfWeek = document.getElementById("day");
+
+  function updateTime() {
+    const d = new Date();
+    const days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+    dayOfWeek.innerHTML = days[d.getDay()];
+    let currentTimeUTC = new Date().toUTCString();
+    utcDisplay.innerHTML = currentTimeUTC;
+  }
+
+  updateTime(); // Initial display
+
+  // Update the time every second (1000 milliseconds)
+  setInterval(updateTime, 1000);
+}
+
+displayTime();
